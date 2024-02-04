9e617488d165ee914756dd527f551b464d087d69
abolfazlrezazadeh <104682369+abolfazlrezazadeh@users.noreply.github.com>
2024-02-04 10:06:58 +0330

    some refactoring

--- app/routes/product.routes.js
+++ app/routes/product.routes.js
@@ -1,70 +1,8 @@
 import {
-  createProduct,
-  getProductById,
-  getProducts,
-} from "../handler/product.handler.js";
-
-const product = {
-  type: "object",
-  properties: {
-    id: {
-      type: "integer",
-    },
-    name: {
-      type: "string",
-    },
-  },
-};
-
-const getProductsItem = {
-  schema: {
-    tags: ["products"],
-    response: {
-      200: {
-        type: "array",
-        items: product,
-      },
-    },
-  },
-  handler: getProducts,
-};
-const getOneProduct = {
-  schema: {
-    tags: ["products"],
-    params: {
-      id: { type: "integer" },
-    },
-    response: {
-      200: {
-        type: "object",
-        properties: { product },
-      },
-    },
-  },
-  handler: getProductById,
-};
-const postProduct = {
-  schema: {
-    tags: ["products"],
-    body: {
-      type: "object",
-      properties: {
-        name: { type: "string" },
-        description: { type: "string" },
-        price: { type: "integer" },
-      },
-    },
-    response: {
-      200: {
-        type: "object",
-        properties: {
-          message: { type: "string" },
-        },
-      },
-    },
-  },
-  handler: createProduct,
-};
+  getOneProduct,
+  getProductsItem,
+  postProduct,
+} from "./Schema/product.schema.js";
 
 export function productRoutes(fastify, options, done) {
   fastify.get("/products", getProductsItem);
